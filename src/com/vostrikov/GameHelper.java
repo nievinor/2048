0c6a6df8dc3951fbc6ec1581bd3d57eba3de7762
Dmitry <applemaycry@mail.ru>
2020-11-27 23:45:21 +0300

    task 2

--- src/com/vostrikov/GameHelper.java
+++ src/com/vostrikov/GameHelper.java
@@ -1,11 +1,38 @@
 package com.vostrikov;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class GameHelper {
 
-    List<Integer> moveAndMergeEqual(List<Integer> list) {
-        return list;
+    public List<Integer> moveAndMergeEqual(List<Integer> list) {
+
+        if (!list.isEmpty() && list.stream().noneMatch(Objects::nonNull)) return list;
+        List<Integer> filteredList = list.stream().filter(Objects::nonNull).collect(Collectors.toList());
+        List<Integer> resultList = new ArrayList<>();
+        int i = 0;
+
+        while (i < filteredList.size()) {
+            Integer currentItem = filteredList.get(i);
+
+            if (i < filteredList.size() - 1 && currentItem.equals(filteredList.get(i + 1))) {
+                resultList.add(currentItem * 2);
+                i = i + 2;
+            } else {
+                resultList.add(currentItem);
+                i++;
+            }
+        }
+
+        if (resultList.size() != list.size()) {
+            int count  = list.size() - resultList.size();
+            for (int j = 0; j < count; j++) {
+                resultList.add(null);
+            }
+        }
+        return resultList;
     }
 
 }
--- src/com/vostrikov/SquareBoard.java
+++ src/com/vostrikov/SquareBoard.java
@@ -1,6 +1,9 @@
 package com.vostrikov;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SquareBoard extends Board {
 
@@ -11,45 +14,77 @@ public class SquareBoard extends Board {
     @Override
     void fillBoard(List<Integer> list) {
 
+        if (list.size() != weight * height) throw new RuntimeException();
+        board.clear();
+        for (int i = 0; i < weight; i++) {
+            for (int j = 0; j < height; j++) {
+                board.put(new Key(i, j), list.get(i * height + j));
+            }
+        }
     }
 
     @Override
     List<Key> availableSpace() {
-        return null;
+
+        List<Key> keys = new ArrayList<>();
+        for (Map.Entry<Key, Integer> entry : board.entrySet()) {
+            if (entry.getValue() == null) {
+                keys.add(entry.getKey());
+            }
+        }
+        return keys;
     }
 
     @Override
     void addItem(Key key, Integer value) {
-
+        board .put(key, value);
     }
 
     @Override
     Key getKey(int i, int j) {
-        return null;
+
+        return board
+                .keySet()
+                .stream()
+                .filter(key -> key.getI() == i && key.getJ() == j)
+                .findFirst()
+                .orElseGet(() -> null);
     }
 
     @Override
     Integer getValue(Key key) {
-        return null;
+        return board.get(key);
     }
 
     @Override
     List<Key> getColumn(int j) {
-        return null;
+        ArrayList<Key> list = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            list.add(getKey(i, j));
+        }
+        return list;
     }
 
     @Override
     List<Key> getRow(int i) {
-        return null;
+        ArrayList<Key> list = new ArrayList<>();
+        for (int j = 0; j < weight; j++) {
+            list.add(getKey(i, j));
+        }
+        return list;
     }
 
     @Override
     boolean hasValue(Integer value) {
-        return false;
+        return board.containsValue(value);
     }
 
     @Override
     List<Integer> getValues(List<Key> keys) {
-        return null;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Key key : keys) list.add(board.get(key));
+
+        return list;
     }
 }
+
